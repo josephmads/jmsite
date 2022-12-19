53e68b0baa0fa2968bc960b8203d3471271b8a00
Joseph Madsen <josephmads@protonmail.com>
2022-12-19 12:25:26 +0000

    fix: removes need for 'os' module in settings.py

--- jmsite/settings.py
+++ jmsite/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import environ
-import os
 from pathlib import Path
 
 
@@ -23,7 +22,7 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/

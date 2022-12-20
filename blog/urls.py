77f098bfed9447242a9024b3d79d20d9a3b12b11
Joseph Madsen <josephmads@protonmail.com>
2022-12-20 14:20:23 +0000

    feat: adds paginate to index

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
     path('<slug:slug>', views.detail, name='detail'),
     path("tags/<int:tag_id>", views.list_tags, name="tag"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 from .models import Post, Tag
 
 
 def home(request):
     return render(request, 'blog/home.html',)
 
-def index(request):
-    posts = Post.objects.all()
-    context = {'posts': posts}
-    return render(request, 'blog/index.html', context)
+# def index(request):
+#     posts = Post.objects.all()
+#     context = {'posts': posts}
+#     return render(request, 'blog/index.html', context)
+
+class Index(generic.ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    ordering = ['-published']
+    paginate_by = 5
 
 def detail(request, slug):
     post = Post.objects.get(slug=slug)

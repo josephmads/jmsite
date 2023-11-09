81a4cf05178087d2f39a50c0e20ed428594b86e9
Joseph Madsen <josephmads@protonmail.com>
2023-11-09 06:26:51 -0600

    adds js page for demos

--- base/static/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-function copyDate() {
-  let footer = document.getElementById("copyright");
-  let copyrightText = "Copyright © Joseph Madsen " + new Date().getFullYear();
-  footer.innerText = copyrightText;
-}
-
-copyDate();
